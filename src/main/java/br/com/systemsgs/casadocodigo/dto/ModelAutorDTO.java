c96588e20d14791fb3344030796b58ce27d6d8a9
GuilhermeJWT <guiromanno@gmail.com>
2021-06-15 11:58:14 -0300

    0-0-3 Email Autor Unico

--- src/main/java/br/com/systemsgs/casadocodigo/dto/ModelAutorDTO.java
+++ src/main/java/br/com/systemsgs/casadocodigo/dto/ModelAutorDTO.java
@@ -5,11 +5,15 @@ import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
 
+import br.com.systemsgs.casadocodigo.annotation.GenericUniqueColumn;
+import br.com.systemsgs.casadocodigo.model.ModelAutor;
+
 public class ModelAutorDTO {
 
 	@NotBlank(message = "O Nome deve ser Informado!!!")
 	private String nome;
 
+	@GenericUniqueColumn(domainClass = ModelAutor.class, fieldName = "email", message = "E-Mail já Cadastrado, Informe Outro!!!")
 	@Email(message = "E-Mail Inválido, Informe Outro!!!")
 	@NotBlank(message = "O E-Mail deve ser Informado!!!")
 	private String email;
