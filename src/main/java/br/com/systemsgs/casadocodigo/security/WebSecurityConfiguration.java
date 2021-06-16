abf9b0660536e506238f94fc9ff6b41530ba59d9
GuilhermeJWT <guiromanno@gmail.com>
2021-06-16 10:28:55 -0300

    Iniciando o UserDetailsService para autenticar o Usuário antes do JWT

--- src/main/java/br/com/systemsgs/casadocodigo/security/WebSecurityConfiguration.java
+++ src/main/java/br/com/systemsgs/casadocodigo/security/WebSecurityConfiguration.java
@@ -1,21 +1,38 @@
 package br.com.systemsgs.casadocodigo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private ImplementacaoUserDetailsService implUserDetailsService;
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
+		auth.userDetailsService(implUserDetailsService)
+			.passwordEncoder(passwordEncoder());
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http.csrf().disable().authorizeRequests()
+		.antMatchers("/api/autor/**").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/api/categoria/**").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/api/livro/**").hasAnyRole("USER", "ADMIN")
+		.and().formLogin();
 	}
 
 }
